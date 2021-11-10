2f37e8b0840648b957e536f8086dc83a5cef9574
BrettLucasM <blucas17@uncc.edu>
2021-11-10 15:26:05 -0500

    Added about page, home page, images, and semi-working menu

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const app = express();
 app.set('view engine', 'ejs');
 fs = require('fs');
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static('assets')); //Calls stylesheet
 
 app.get('/', function (req, res) {
 
-    res.render('home');
+    //res.render('home');
     console.log('Request was made: ' + req.url);
+    var v = 0;
+    let data ={
+        v: v
+    }
+    res.render('home', {data: data});
+});
+
+app.get('/About', function (req, res) {
+
+    //res.render('home');
+    console.log('Request was made: ' + req.url);
+    var v = 1;
+    let data ={
+        v: "initial"
+    }
+    res.render('home', {data: data});
 });
 
 
